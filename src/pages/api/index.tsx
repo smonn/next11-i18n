2b8b81efbfcd89d0884f0675f722e558f1d7e662
Simon Ingeson <44818+smonn@users.noreply.github.com>
2021-06-18 20:57:43 -0400

    test NEXT_LOCALE cookie

--- src/pages/api/index.tsx
+++ src/pages/api/index.tsx
@@ -5,7 +5,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const locale = req.query.__nextLocale as string;
+  const locale = req.cookies.NEXT_LOCALE || (req.query.__nextLocale as string);
   const t = await getT(locale, "common");
   res.json({
     appName: t("common:appName"),
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,17 +3,20 @@ import useTranslation from "next-translate/useTranslation";
 import { useEffect } from "react";
 import { GetServerSideProps, GetServerSidePropsContext } from "next";
 import getT from "next-translate/getT";
+import { useRouter } from "next/router";
 
 export default function HomePage({ appName }: { appName: string }) {
   const { t } = useTranslation();
+  const { locale } = useRouter();
 
   useEffect(() => {
-    fetch("/api")
+    document.cookie = `NEXT_LOCALE=${locale}; SameSite=Lax`;
+    fetch(`/api`)
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
       });
-  }, []);
+  }, [locale]);
 
   return (
     <>
