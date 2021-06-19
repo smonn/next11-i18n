5946016a249c5c3e4be4c1764921657a742b01d8
Simon Ingeson <44818+smonn@users.noreply.github.com>
2021-06-18 20:13:57 -0400

    setup server side props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
 import Head from "next/head";
 import useTranslation from "next-translate/useTranslation";
 import { useEffect } from "react";
+import { GetServerSideProps, GetServerSidePropsContext } from "next";
+import getT from "next-translate/getT";
 
-export default function HomePage() {
+export default function HomePage({ appName }: { appName: string }) {
   const { t } = useTranslation();
 
   useEffect(() => {
@@ -19,6 +21,16 @@ export default function HomePage() {
         <title>{t("common:appName")}</title>
       </Head>
       <h1>{t("home:pageTitle")}</h1>
+      <p>App Name from props: {appName}</p>
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const t = await getT(context.locale, "common");
+  return {
+    props: {
+      appName: t("appName"),
+    },
+  };
+};
